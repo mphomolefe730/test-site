b1a01d055bedd48fe4fa711f6341ef8937addf75
amogelang chaane <amogelangchaane200@gmail.com>
2023-09-21 00:09:38 +0200

    deleted some files and made the home component the main one(the one being used inside the root index.html file)

--- site/src/app/app.module.ts
+++ site/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CarouselComponent } from './components/testimonial/testimonial.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,7 +12,6 @@ import { PackageModalComponent } from './components/package-modal/package-modal.
 
 @NgModule({
   declarations: [
-    AppComponent,
     HomeComponent,
     CarouselComponent,
     NavDesktopComponent,
@@ -28,6 +26,6 @@ import { PackageModalComponent } from './components/package-modal/package-modal.
     BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomeComponent]
 })
 export class AppModule { }
--- site/src/app/components/navBar/nav-bar.component.ts
+++ site/src/app/components/navBar/nav-bar.component.ts
@@ -1,32 +1,26 @@
-import { Component, HostListener, ElementRef, AfterViewInit, Renderer2 } from '@angular/core';
+import { Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavComponent implements AfterViewInit {
+export class NavComponent {
   menuOpen = false;
   dropdown = false;
   public screenWidth: any = window.innerWidth;
   navbarSolid = 0;
-
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   @HostListener('window:resize', ['$event'])
   onResize(event: any) {
     this.screenWidth = window.innerWidth;
   }
 
-  ngAfterViewInit() {
-    const bodies = this.el.nativeElement.ownerDocument.querySelectorAll('body');
-    const lastBody = bodies[bodies.length - 1];
-
-    this.renderer.listen(lastBody, 'scroll', (event) => {
-      this.navbarSolid = event.target.scrollTop;
-      
-    });
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: any) {
+    this.navbarSolid = window.scrollY;
   }
+
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
