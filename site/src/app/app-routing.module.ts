d0ca3b8f202b3962906e593ba1d77ae9fcc3d1a5
mphomolefe730 <mphomolefe730@gmail.com>
2023-09-22 10:32:28 +0200

    fixed marge conflict

--- site/src/app/app-routing.module.ts
+++ site/src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-=======
-import { PackagesViewComponent} from './explore/packages-view/packages-view.component'
+// import { PackagesViewComponent} from './explore/packages-view/packages-view.component'
 import { AboutComponent } from './about/about.component';
-=======
 
 const routes: Routes = [
   {path:'', redirectTo:'/home', pathMatch:'full'},
