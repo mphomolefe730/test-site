ac6eb5f64c83c95bcc55aefd83c8313f54663fe2
mphomolefe730 <mphomolefe730@gmail.com>
2023-09-23 00:48:45 +0200

    start animation before the app starts

--- site/src/app/app.component.ts
+++ site/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  screenWidth = screen.width>700;
+export class AppComponent implements AfterViewInit{
+  loader=true;
+  logo='../favicon.ico';
+
+  ngAfterViewInit() {
+    // this.loader = false;
+  }
+  ngOnInit():void{
+    setTimeout(() => {
+      this.loader = false
+    }, 7000);
+
+  }
 }
